e55340e84b3446e4a4f09bff3032da51b2afd94e
hyeppyday <tajokoung@gmail.com>
2023-10-17 16:02:27 +0900

    20231017

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/common.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/common.js
@@ -11,14 +11,6 @@ import { startSS, setPos } from "./smoothScroll23.js";
 // 부드러운 스크롤 적용 //////////
 startSS();
 
-// 0. 새로고치면 스크롤바 위치캐싱후 맨위로 이동
-setTimeout(() => {
-    // 윈도우 스크롤 맨위로!
-    window.scrollTo(0, 0);
-    // 부드러운 스크롤 위치값 반영!
-    setPos(0);
-    // 안하면 원래 위치로 스크롤시 튐!
-  }, 400);
   // 0. 스크롤바 트랙을 잡고 위치이동시 위치값 반영
   dFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
   //////// mouseup /////////////
@@ -31,11 +23,12 @@ setTimeout(() => {
 // 대상선정: .common-area
 const comArea = dFn.qsa('.common-area');
 
-console.log(tData,comArea);
+// console.log(tData,comArea);
 
 // 상단영역 html 넣기
 comArea[0].innerHTML = tData.topArea;
 // 하단영역 html 넣기
 comArea[1].innerHTML = tData.footerArea;
+
 
 
