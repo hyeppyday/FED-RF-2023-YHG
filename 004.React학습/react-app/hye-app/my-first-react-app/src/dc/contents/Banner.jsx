2e9a2848309bd4630a81d0424486e6403eb2969c
hyeppyday <tajokoung@gmail.com>
2023-11-09 18:19:11 +0900

    20231109

---
+++ 004.React학습/react-app/hye-app/my-first-react-app/src/dc/contents/Banner.jsx
@@ -0,0 +1,28 @@
+// DC.com 배너 컴포넌트
+
+// 배너데이터
+import { banData } from "../data/banner.js";
+// 배너 CSS
+import '../../css/banner.css';
+
+// 배너 컴포넌트
+ export function Banner(props){
+    // category - 카테고리 분류명(배너데이터 선택)
+// 리스트 만들기 함수
+const makeList = (data) =>{
+    console.log(data);
+    return data.map((v,i)=>(
+    <li key={i}>
+        <img src={v.src} alt="하하"/>
+    </li>))
+};
+
+    return(
+        <div className="banner">
+            {/* 이동슬라이드 */}
+            <ul className="slider">
+                    {makeList(banData[props.category])}
+            </ul>
+        </div>
+    )
+ }/////////// Banner 컴포넌트 ////////////
---
+++ 004.React학습/react-app/hye-app/my-first-react-app/src/dc/layout/TopArea.jsx
@@ -0,0 +1,42 @@
+// 상단영역 컴포넌트
+// GNB 데이터
+import { menu } from "../data/gnb";
+import { Logo } from "../contents/Logo";
+
+export function TopArea(props){
+    return(
+        <>
+        {/* 1. 상단영역 */}
+        <header className="top-area">
+            {/* 네비게이션 GNB파트 */}
+            <nav className="gnb">
+                <ul>
+                    <li>
+                        <Logo/>
+                        </li>
+                    {
+                        
+                        menu.map((v,i)=>
+                        <li key={i}>
+                            <a href="#" 
+                            onClick={()=>props.chgFn(
+                                v.txt=='Home'?'main':v.txt
+                                )}>{v.txt}</a>
+                        </li>
+                            /* 
+                            map()을 사용하여 태그를 생성할때
+                            데이터의 유일키를 key속성으로 만들지않으면
+                            아래와 같은 에러가 발생함!!
+                            (이유: 구별되는 항목으로 나중에 업데이트시 
+                            이용할 수 있도록 리액트에서 강제하고 있음)
+
+                            Warning: Each child in a list should have a unique "key" prop.
+                            */
+                            )
+                    }
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
