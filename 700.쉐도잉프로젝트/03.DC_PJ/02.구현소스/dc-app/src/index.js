65dcd1ea25098235a1d43d753b722acee3e361d9
hyeppyday <tajokoung@gmail.com>
2023-12-05 11:57:06 +0900

    1

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -5,7 +5,7 @@ import './css/index.css';
 
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 import { Layout } from "./components/layout/Layout";
@@ -57,14 +57,8 @@ import { Member } from './components/pages/Member';
 // 출력해야하기 때문에 스스로 내보내기를 셋팅해야하는 것!
 export default function App() {
   return (
-
-    /* basename속성은 package.json의 "homepage"속성값을 읽어옴 */
     
-
-    /* basename 을 안써도 HashRouter는 package.json의 homepage 속성값을 
-    자동으로 연결함 */
-    // <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <BrowserRouter>
+  <HashRouter>
     <Routes>
       {/* 중요!!! 레이아웃 컴포넌트를 루트로 설정! */}
       <Route path="/" element={<Layout/>}>
@@ -85,8 +79,7 @@ export default function App() {
         <Route path='member' element={<Member/>}/>
       </Route>
     </Routes>
-    </BrowserRouter>
-  
+  </HashRouter>
   )
 } //////////////// App 컴포넌트 //////////////////
 
