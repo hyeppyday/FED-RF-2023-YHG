2726ad8cf9f928c5a697cc201cdb648d22f0053c
hyeppyday <tajokoung@gmail.com>
2023-12-05 12:05:36 +0900

    1

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -57,8 +57,14 @@ import { Member } from './components/pages/Member';
 // 출력해야하기 때문에 스스로 내보내기를 셋팅해야하는 것!
 export default function App() {
   return (
+
+    /* basename속성은 package.json의 "homepage"속성값을 읽어옴 */
+    // <BrowserRouter basename={process.env.PUBLIC_URL}>
+    // <BrowserRouter>
     
-  <HashRouter>
+    /* basename 을 안써도 HashRouter는 package.json의 homepage 속성값을 
+    자동으로 연결함 */
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       {/* 중요!!! 레이아웃 컴포넌트를 루트로 설정! */}
       <Route path="/" element={<Layout/>}>
@@ -79,7 +85,7 @@ export default function App() {
         <Route path='member' element={<Member/>}/>
       </Route>
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
   )
 } //////////////// App 컴포넌트 //////////////////
 
