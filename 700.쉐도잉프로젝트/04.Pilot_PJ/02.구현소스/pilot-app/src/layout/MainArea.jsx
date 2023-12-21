845ab44c9ef2a7cd4c914c1494bf478a19afa5f5
hyeppyday <tajokoung@gmail.com>
2023-12-21 12:00:24 +0900

    20231221

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/layout/MainArea.jsx
@@ -2,11 +2,16 @@
 
 import { MainCont } from "../pages/MainCont";
 import { Fashion } from "../pages/Fashion";
+import { GList } from "../pages/GList";
 
 // 라우터 역할을 하는 MainArea컴포넌트 //////////
 export function MainArea(props) {
   // props.page 속성값으로 main/men/women/style
   return (
-    <>{props.page == "main" ? <MainCont /> : <Fashion cat={props.page} />}</>
+    // main or glist가 아니면 서브 Fashion이동
+    <>{
+      props.page == "main" ? <MainCont /> :
+      props.page == "glist" ? <GList /> :
+     <Fashion cat={props.page} />}</>
   );
 } /////////////// MainArea 컴포넌트 /////////////////
